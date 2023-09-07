be5d98ac4c56738a9785e61cd846acdf6ea5b73d
larrytabeh <larrytabeh@gmail.com>
2023-09-07 19:16:50 +0100

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -4,7 +4,7 @@ let productsHTML = '';
 
 
 products.forEach((product, index) => {
-    productsHTML  +=
+    productsHTML +=
         `
         <div class="product-container">
           <div class="product-image-container">
@@ -50,7 +50,7 @@ products.forEach((product, index) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -59,4 +59,40 @@ products.forEach((product, index) => {
 
 });
 
-document.querySelector(".js-products-grid").innerHTML = productsHTML
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            let matchingItem;
+
+            cart.forEach((item) => {
+                if (productId === item.productId) {
+                    matchingItem = item;
+                }
+            });
+
+            if (matchingItem) {
+                matchingItem.quantity += 1;   
+            } else {
+
+                cart.push({
+                    productId, quantity: 1
+
+                });
+
+            }
+            let cartQuantity = 0;
+            cart.forEach((item)=>{
+               cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+           
+        });
+
+
+    });
+
